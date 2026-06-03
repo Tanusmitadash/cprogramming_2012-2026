703e0129374e57afcc3398dc2458659c51e86c1d
Tanusmitadash <49684716+Tanusmitadash@users.noreply.github.com>
2026-06-03 22:18:03 +0530

    Update countfrequencyofelements_5.c

--- countfrequencyofelements_5.c
+++ countfrequencyofelements_5.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int arr[] = {1, 2, 2, 3, 1, 4, 2};
+    int n = 7;
+    int visit[7] = {0};
+
+    for(int i = 0; i < n; i++) {
+
+        if(visit[i] == 1)
+            continue;
+
+        int count = 1;
+/*Nested loops are used:O(n2)*/
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                visit[j] = 1;
+            }
+        }
+
+        printf("%d occurs %d times\n", arr[i], count);
+    }
+
+    return 0;
+}
