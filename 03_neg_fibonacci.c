e504698637415c9fa171067a576c6a32b16573a5
Tanusmitadash <49684716+Tanusmitadash@users.noreply.github.com>
2026-07-30 11:32:09 +0530

    Add files via upload

---
+++ 03_neg_fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a = 0, b = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Negative Fibonacci series:\n");
+
+    printf("%d %d ", a, b);
+
+    for (i = 2; i < n; i++)
+    {
+        next = a - b;
+        printf("%d ", next);
+
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
